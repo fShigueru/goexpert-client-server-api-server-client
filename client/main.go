d11e86c94b88e5560f96487d1da42e06a9a92d73
fshigueru <filipe.shigueru@yahoo.com.br>
2023-05-26 19:47:08 -0300

    primeiro commit

---
+++ client/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Dolar struct {
+	Dolar string `json:"dolar"`
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao cria um novo request: %v\n", err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao fazer a requisição: %v\n", err)
+	}
+	defer res.Body.Close()
+
+	resbody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao ler a requisição: %v\n", err)
+	}
+
+	var dolar Dolar
+	dolar.Dolar = string(resbody)
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao criar o arquivo: %v\n", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("Dólar:{%s}", dolar.Dolar))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao persistir o valor no arquivo: %v\n", err)
+	}
+
+	fmt.Println("Cotação salva com sucesso")
+}
---
+++ server/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type RealDolarResponse struct {
+	Usdbrl struct {
+		Code       string `json:"code"`
+		Codein     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	} `json:"USDBRL"`
+}
+
+type RealDolar struct {
+	ID         int    `gorm:"primaryKey"`
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+	gorm.Model
+}
+
+func main() {
+	println("Server :8080")
+	http.HandleFunc("/cotacao", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	realDolar, err := BuscaCambioRealDolar()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(realDolar.Usdbrl.Bid)
+}
+
+func BuscaCambioRealDolar() (*RealDolarResponse, error) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		return nil, err
+	}
+	//em vez de criar um Client, podemos usar um default
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var realDolarResponse RealDolarResponse
+	err = json.Unmarshal(body, &realDolarResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	Sqlite(&realDolarResponse)
+	return &realDolarResponse, nil
+}
+
+func Sqlite(realDolarResponse *RealDolarResponse) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+
+	db, err := gorm.Open(sqlite.Open("db/realdolar.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	println("Conectando sqlite3")
+	db.AutoMigrate(&RealDolar{})
+
+	usdbrl := realDolarResponse.Usdbrl
+	db.WithContext(ctx).Create(&RealDolar{
+		Code:       usdbrl.Code,
+		Codein:     usdbrl.Codein,
+		Name:       usdbrl.Name,
+		High:       usdbrl.High,
+		Low:        usdbrl.Low,
+		VarBid:     usdbrl.VarBid,
+		PctChange:  usdbrl.PctChange,
+		Bid:        usdbrl.Bid,
+		Ask:        usdbrl.Ask,
+		Timestamp:  usdbrl.Timestamp,
+		CreateDate: usdbrl.CreateDate,
+	})
+
+	// var realDolarBusca []RealDolar
+	// db.Find(&realDolarBusca)
+	// for _, realDolar := range realDolarBusca {
+	// 	fmt.Println(realDolar.Name, realDolar.High, realDolar.Low)
+	// }
+}
